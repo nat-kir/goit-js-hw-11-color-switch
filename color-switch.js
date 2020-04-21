508f2dd3605d93dc3152eefc3196c3a55256cc54
Natalia Kiriunina <kiriunina.natalia@gmail.com>
2020-04-21 03:05:07 +0300

    hw-11-task-1 git commit -m hw-11-task-1 git commit -m hw-11-task-1

--- color-switch.js
+++ color-switch.js
@@ -1,3 +1,35 @@
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+
+
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+const bodyRef = document.querySelector("body");
+
+startBtnRef.disabled = false;
+let intervalId = null;
+
+startBtnRef.addEventListener("click", startColorSwitch);
+stopBtnRef.addEventListener("click", stopColorSwitch);
+
+function startColorSwitch() {
+  intervalId = setInterval(() => {
+       bodyRef.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+    startBtnRef.disabled = true;
+
+  }, 1000);
+}
+
+function stopColorSwitch() {
+  clearInterval(intervalId);
+  startBtnRef.disabled = false;
+  bodyRef.style.backgroundColor = "#FFFFFF";
+}
